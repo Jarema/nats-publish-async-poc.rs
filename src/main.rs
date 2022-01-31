39d8cbbe983b97632034cb2639811455d85c5531
Jarema <melgaer@gmail.com>
2022-01-31 13:17:43 +0100

    Fixed performance

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use std::sync::Arc;
 use std::time::Duration;
 
 use bytes::BufMut;
+use parking_lot::lock_api::RawMutex;
 use tokio::io::AsyncBufReadExt;
 use tokio::io::AsyncWriteExt;
 use tokio::io::BufReader;
+use tokio::io::BufWriter;
 use tokio::net::tcp::OwnedWriteHalf;
 use tokio::net::TcpStream;
 use tokio::sync::Mutex;
@@ -35,14 +37,19 @@ fn main() {
 }
 
 struct Connection {
-    writer: Arc<Mutex<OwnedWriteHalf>>,
+    writer: Arc<Mutex<BufWriter<OwnedWriteHalf>>>,
 }
 
 impl Connection {
     async fn connect() -> Connection {
-        let con = TcpStream::connect("localhost:4222").await.unwrap();
+        let socket = tokio::net::TcpSocket::new_v4().unwrap();
+        let addr = "127.0.0.1:4222".parse().unwrap();
+        // socket.set_send_buffer_size( 1024).unwrap();
+        let con = socket.connect(addr).await.unwrap();
+        con.set_nodelay(false).unwrap();
         let (read, writer) = con.into_split();
         let mut read = BufReader::new(read);
+        let mut writer = BufWriter::new(writer);
 
         let writer = Arc::new(Mutex::new(writer));
         writer.lock().await.write_all(b"CONNECT { \"no_responders\": true, \"headers\": true, \"verbose\": false, \"pedantic\": false }\r\n").await.unwrap();
@@ -84,24 +91,40 @@ impl Connection {
             Op::Raw(data) => self.writer.lock().await.write_all(&data).await.unwrap(),
             Op::Flush => self.writer.lock().await.flush().await.unwrap(),
             Op::Publish(subject, payload) => {
-                let mut buf = bytes::BytesMut::with_capacity(payload.len() + subject.len());
-                buf.put(&b"PUB "[..]);
-                buf.put(subject.as_bytes());
-                buf.put(&b" "[..]);
+                let mut writer = self.writer.lock().await;
+                writer.write_all(b"PUB ").await.unwrap();
+                writer.write_all(subject.as_bytes()).await.unwrap();
+                writer.write_all(b" ").await.unwrap();
 
-                let mut buflen = itoa::Buffer::new();
-                buf.put(buflen.format(payload.len()).as_bytes());
-                buf.put(&b"\r\n"[..]);
-
-                buf.put(&*payload);
-                buf.put(&b"\r\n"[..]);
-
-                self.writer
-                    .lock()
-                    .await
-                    .write_all_buf(&mut buf)
+                let mut bufi = itoa::Buffer::new();
+                writer
+                    .write_all(bufi.format(payload.len()).as_bytes())
                     .await
                     .unwrap();
+                writer.write_all(b"\r\n").await.unwrap();
+
+                writer.write_all(payload).await.unwrap();
+                writer.write_all(b"\r\n").await.unwrap();
+
+                // let mut buf = bytes::BytesMut::with_capacity(payload.len() + subject.len());
+
+                // buf.put(&b"PUB "[..]);
+                // buf.put(subject.as_bytes());
+                // buf.put(&b" "[..]);
+
+                // let mut buflen = itoa::Buffer::new();
+                // buf.put(buflen.format(payload.len()).as_bytes());
+                // buf.put(&b"\r\n"[..]);
+
+                // buf.put(&*payload);
+                // buf.put(&b"\r\n"[..]);
+
+                // self.writer
+                //     .lock()
+                //     .await
+                //     .write_all_buf(&mut buf)
+                //     .await
+                //     .unwrap();
             }
         }
     }
